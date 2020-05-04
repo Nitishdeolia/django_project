436f195844afa87bde6d49a9a60125ade0ec8ebd
Nitishdeolia <61326045+Nitishdeolia@users.noreply.github.com>
2020-05-04 15:03:02 +0530

    made search model

--- webscrapping_app/myapp/models.py
+++ webscrapping_app/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+# Search model
+
+class Search(models.Model):
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
